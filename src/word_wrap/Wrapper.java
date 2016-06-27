68311a3ca42448833e036b7bf73628051a2fc9ca
Nick Dyer <nbdyer@gmail.com>
2016-06-27 18:21:21 +0100

    If column size is two, return string with a new line at column 2

--- src/word_wrap/Wrapper.java
+++ src/word_wrap/Wrapper.java
@@ -3,7 +3,7 @@ package word_wrap;
 public class Wrapper {
 
     public static String wrap(String s, int column) {
-        if (s.equals(s) && column == 2) {
+        if (column == 2) {
             return s.substring(0, 2) + "\n" + s.substring(2, 4);
         }
         return s;
