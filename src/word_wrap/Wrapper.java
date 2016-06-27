fb50d6993ea8efbd1436869ff24d5bdc239b1e31
Nick Dyer <nbdyer@gmail.com>
2016-06-27 17:55:52 +0100

    Removes raised exception and just returns s

--- src/word_wrap/Wrapper.java
+++ src/word_wrap/Wrapper.java
@@ -1,13 +1,8 @@
 package word_wrap;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 public class Wrapper {
 
     public static String wrap(String s, int column) {
-        if (true) {
-            return s;
-        }
-        throw new NotImplementedException();
+        return s;
     }
 }
