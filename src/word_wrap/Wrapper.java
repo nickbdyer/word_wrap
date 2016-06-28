8286aed7f3caa91b9e17506127926e70ea878207
Nick Dyer <nbdyer@gmail.com>
2016-06-28 11:39:08 +0100

    Last refactoring step added functionality, so reverted back

--- src/word_wrap/Wrapper.java
+++ src/word_wrap/Wrapper.java
@@ -3,7 +3,10 @@ package word_wrap;
 public class Wrapper {
 
     public static String wrap(String s, int column) {
-        if (column < s.length()) {
+        if (column > 1 && column < s.length()) {
+            return s.substring(0, column) + "\n" + s.substring(column, s.length());
+        }
+        if (column > 3 && column < s.length()) {
             return s.substring(0, column) + "\n" + s.substring(column, s.length());
         }
         return s;
