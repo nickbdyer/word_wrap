07cfe069e9b0538f91201ea3eed966a5bbe47300
Nick Dyer <nbdyer@gmail.com>
2016-06-28 10:02:37 +0100

    Reverse if statement for better readability

--- src/word_wrap/Wrapper.java
+++ src/word_wrap/Wrapper.java
@@ -3,7 +3,7 @@ package word_wrap;
 public class Wrapper {
 
     public static String wrap(String s, int column) {
-        if (column < 4 && column > 1) {
+        if (column > 1 && column < 4) {
             return s.substring(0, column) + "\n" + s.substring(column, 4);
         }
         return s;
