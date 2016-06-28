af4c05ebbf72eb604dac3b69d29fdc8e1f533145
Nick Dyer <nbdyer@gmail.com>
2016-06-28 09:07:47 +0100

    Refactor to single if statement on column int

--- src/word_wrap/Wrapper.java
+++ src/word_wrap/Wrapper.java
@@ -3,11 +3,8 @@ package word_wrap;
 public class Wrapper {
 
     public static String wrap(String s, int column) {
-        if (column == 2) {
-            return s.substring(0, 2) + "\n" + s.substring(2, 4);
-        }
-        if (column == 3) {
-            return s.substring(0, 3) + "\n" + s.substring(3, 4);
+        if (column == 2 || column == 3) {
+            return s.substring(0, column) + "\n" + s.substring(column, 4);
         }
         return s;
     }
