c93ac7a65a9c68d7d5d4c1ea6ea876619bc299ca
Nick Dyer <nbdyer@gmail.com>
2016-06-28 08:58:50 +0100

    Refactor to remove hardcoded words with wrap at column 3

--- src/word_wrap/Wrapper.java
+++ src/word_wrap/Wrapper.java
@@ -6,11 +6,8 @@ public class Wrapper {
         if (column == 2) {
             return s.substring(0, 2) + "\n" + s.substring(2, 4);
         }
-        if (column == 3 && s.equals("four")) {
-            return "fou\nr";
-        }
         if (column == 3) {
-            return "wor\nd";
+            return s.substring(0, 3) + "\n" + s.substring(3, 4);
         }
         return s;
     }
