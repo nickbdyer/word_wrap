3a0d2d329458456fdfe68cc6fe8259cfb931d257
Nick Dyer <nbdyer@gmail.com>
2016-06-28 11:25:28 +0100

    Remove lower bound check for substring check

--- src/word_wrap/Wrapper.java
+++ src/word_wrap/Wrapper.java
@@ -3,7 +3,7 @@ package word_wrap;
 public class Wrapper {
 
     public static String wrap(String s, int column) {
-        if (column > 1 && column < s.length()) {
+        if (column < s.length()) {
             return s.substring(0, column) + "\n" + s.substring(column, s.length());
         }
         if (column > 3 && column < s.length()) {
