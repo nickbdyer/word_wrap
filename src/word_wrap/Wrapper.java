11598b74c5fa7c34ed96ec4fd01e9bfe983ba1a9
Nick Dyer <nbdyer@gmail.com>
2016-06-28 12:53:04 +0100

    Adds recursive call to wrap one word multiple times

--- src/word_wrap/Wrapper.java
+++ src/word_wrap/Wrapper.java
@@ -3,9 +3,9 @@ package word_wrap;
 public class Wrapper {
 
     public static String wrap(String s, int column) {
-        if (s.length() < column) {
+        if (s.length() <= column) {
             return s;
         }
-        return s.substring(0, column) + "\n" + s.substring(column, s.length());
+        return s.substring(0, column) + "\n" + wrap(s.substring(column, s.length()), column);
     }
 }
