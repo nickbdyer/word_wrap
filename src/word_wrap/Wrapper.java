974f938d9f903dd5c0d9fa7605185b488e70105c
Nick Dyer <nbdyer@gmail.com>
2016-06-28 11:26:23 +0100

    Removes duplicated substring statement

--- src/word_wrap/Wrapper.java
+++ src/word_wrap/Wrapper.java
@@ -6,9 +6,6 @@ public class Wrapper {
         if (column < s.length()) {
             return s.substring(0, column) + "\n" + s.substring(column, s.length());
         }
-        if (column > 3 && column < s.length()) {
-            return s.substring(0, column) + "\n" + s.substring(column, s.length());
-        }
         return s;
     }
 }
